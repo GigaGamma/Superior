cbd6f7023ecf15a152b712a167fe26a9f6823dbf
ThePenguinGamerz <brianminecraftpenguin@gmail.com>
2017-11-22 11:43:05 -0500

    Added YT2

---
+++ Superior/src/network/superior/ranks/YouTuber2.java
@@ -0,0 +1,43 @@
+/*
+ * SuperiorNetworks tier 2 YT rank
+ */
+package network.superior.ranks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.permissions.Permission;
+
+import link.mc.permission.Rank;
+
+public class YouTuber2 implements Rank {
+
+	@Override
+	public String getName() {
+		return "YouTuber";
+	}
+
+	@Override
+	public String getId() {
+		return "yt";
+	}
+
+	@Override
+	public ChatColor getColor() {
+		return ChatColor.AQUA;
+	}
+
+	@Override
+	public boolean isOpped() {
+		return false;
+	}
+
+	@Override
+	public List<Permission> getPerms() {
+		List<Permission> p = new ArrayList<Permission>();
+		p.add(new Permission("mclink.fly"));
+		return p;
+	}
+
+}
